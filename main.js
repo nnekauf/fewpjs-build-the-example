a5c5138c1508875dbab10bdf31a2e9110e256731
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-27 03:10:02 -0400

    commit tree 86cb35ea7bc532da47b64f9ced0f67410030594c

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.querySelector('#modal')
 
+function hideModal() {
+  modal.setAttribute('class', 'hidden')
+}
 
+const hearts = document.getElementsByClassName("like-glyph")
 
+  for (const heart of hearts){
+    heart.addEventListener("click", function(e) {
+      mimicServerCall()
+      .then(function(response) {
+        if (heart.innerHTML == EMPTY_HEART){
+          heart.innerHTML = FULL_HEART
+          heart.className = "activated-heart"
+        } else {
+        heart.innerHTML = EMPTY_HEART
+        heart.className = "like-glyph"
+        }
+      })
+      .catch(function(error) {
+        modal.classList.remove('hidden')
+        let message = document.querySelector("modal-message")
+        message.innerHTML = error
+        setTimeout(() => {
+          modal.hidden = true}, 5000)
+        }); 
+    })
+  }
+
+hideModal()
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
